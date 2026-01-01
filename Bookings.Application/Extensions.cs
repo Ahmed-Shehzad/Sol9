91fa24066506f73379dd7415d7f8078eabb67d1f
Muhammad Ahmed Shehzad <ahmedshehzad786@gmail.com>
2026-01-01 22:34:50 +0100

    refactor: Remove Verifier registration from application extensions

--- Bookings.Application/Extensions.cs
+++ Bookings.Application/Extensions.cs
@@ -2,8 +2,6 @@ using Intercessor;
 
 using Microsoft.Extensions.DependencyInjection;
 
-using Verifier;
-
 namespace Bookings.Application;
 
 public static class Extensions
@@ -13,10 +11,6 @@ public static class Extensions
         _ = services.AddIntercessor(options =>
         {
             options.RegisterFromAssembly(typeof(BookingsApplication).Assembly);
-        });
-        _ = services.AddVerifier(builder =>
-        {
-            builder.RegisterFromAssembly(typeof(BookingsApplication).Assembly);
         });
         return services;
     }
--- Orders.Application/Extensions.cs
+++ Orders.Application/Extensions.cs
@@ -2,8 +2,6 @@
 
 using Microsoft.Extensions.DependencyInjection;
 
-using Verifier;
-
 namespace Orders.Application;
 
 public static class Extensions
@@ -13,10 +11,6 @@ public static class Extensions
         _ = services.AddIntercessor(options =>
         {
             options.RegisterFromAssembly(typeof(OrdersApplication).Assembly);
-        });
-        _ = services.AddVerifier(builder =>
-        {
-            builder.RegisterFromAssembly(typeof(OrdersApplication).Assembly);
         });
         return services;
     }
